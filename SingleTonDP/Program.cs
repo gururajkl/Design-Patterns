7d696f54a27615e756abee775c613ac304b9dbe0
GururajKL <gururaj.kl@sasken.com>
2023-09-07 17:05:46 +0530

    Add project files.

---
+++ SingleTonDP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SingleTonDP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTon s1 = SingleTon.GetInstanceAsSingleTonForThisClass;
+            s1.PrintMessage("S1 message");
+
+            SingleTon s2 = SingleTon.GetInstanceAsSingleTonForThisClass;
+            s2.PrintMessage("S2 message");
+        }
+    }
+}
---
+++ SingleTonDP/SingleTon.cs
@@ -0,0 +1,43 @@
+﻿namespace SingleTonDP
+{
+    /// <summary>
+    /// Sealed class that prevents the inheritance and avoid multiple objects for the entire project.
+    /// </summary>
+    public sealed class SingleTon
+    {
+        private static int counter = 0;
+        // This static varibale creates only one instance for this class.
+        private static SingleTon? instance = null;
+
+        private SingleTon()
+        {
+            counter++;
+            Console.WriteLine(counter);
+        }
+
+        /// <summary>
+        /// Public property helps to get the instance of this class from the instance local variable,
+        /// Which gets the new instance only if that variable is null.
+        /// </summary>
+        public static SingleTon GetInstanceAsSingleTonForThisClass
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new SingleTon();
+                }
+                return instance;
+            }
+        }
+
+        /// <summary>
+        /// Prints the passed message from the object.
+        /// </summary>
+        /// <param name="message">Anything can be passed.</param>
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
